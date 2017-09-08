63577db0d477f8ea7209a635c83cc3c3b14580fe
micnori <micnori@yahoo.com>
2017-09-08 16:59:51 +0200

    engine first import

---
+++ cedus-engine/src/main/java/it/smartcommunitylab/cedus/common/Const.java
@@ -0,0 +1,7 @@
+package it.smartcommunitylab.cedus.common;
+
+public class Const {
+	public static final String ERRORTYPE = "errorType";
+	public static final String ERRORMSG = "errorMsg";
+	
+}
---
+++ cedus-engine/src/main/java/it/smartcommunitylab/cedus/storage/RepositoryManager.java
@@ -0,0 +1,19 @@
+package it.smartcommunitylab.cedus.storage;
+
+import it.smartcommunitylab.cedus.model.stats.RegistrationStats;
+
+import java.util.List;
+
+public class RepositoryManager {
+
+	public List<RegistrationStats> getRegistrationStatsByLevel(List<String> years, List<String> levels) {
+		//TODO
+		return null;
+	}
+	
+	public List<RegistrationStats> getRegistrationStatsByType(List<String> years, List<String> types) {
+		//TODO
+		return null;
+	}
+	
+}
