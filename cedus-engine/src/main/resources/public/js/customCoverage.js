c829d92a93d38fdc9902b3eb1e6f43b7d004af10
labu4bd <labu4bd@gmail.com>
2017-10-26 11:56:19 +0200

    add type and time of distance in Coverage
    add type and time of distance in Coverage. change the color of polygon.

--- cedus-engine/src/main/resources/public/js/customCoverage.js
+++ cedus-engine/src/main/resources/public/js/customCoverage.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 		success: function (data) {
 		//console.log("data name:",data);
 		$.each(data,function(key, val){
-			console.log('dd:',val);
+			//console.log('dd:',val);
 			var strVal="\"change_div('"+val+"')\"";
-			console.log('strVal:',strVal)
+			//console.log('strVal:',strVal)
 			$('#select_btn').append("<div class='row'><button type='button' class='btn btn-primary btn-block' style='width:90%' onclick="+strVal+" >"+val+"</button></div><br/>");
 		});
 		
@@ -33,7 +33,7 @@ $(document).ready(function() {
 	    		//dataType : 'json',
 	    		//url: 'https://dev.smartcommunitylab.it/cedus/api/cover/education',
 	    		url:'../api/cover/education',
-	    		data: {ordine:$('#levelText').text(),tipologia:$(this).val(),filter:'TRANSIT_DISTANCE'} ,
+	    		data: {ordine:$('#levelText').text(),tipologia:$(this).val(),filter:'CAR_DISTANCE'} ,
 	    		success: function (data) {
 	    		//console.log("data name:",data['tuList']);
 	    		$.each(data['tuList'],function(key, val){
@@ -51,13 +51,53 @@ $(document).ready(function() {
 	    	});
 		}
     });
+	$( "input.radioOption" ).on( "click", function() {
+		console.log( " is checked:" ,$( "input.radioOption:checked" ).val());
+		//change range table below
+		var radioValSelect=$( "input.radioOption:checked" ).val();
+		
+		if(radioValSelect=="CAR_DISTANCE" || radioValSelect=="TRANSIT_DISTANCE"){
+			$('#timeMark').hide();
+			$('#distanceMark').show();
+		}else if(radioValSelect=="CAR_TIME" || radioValSelect=="TRANSIT_TIME"){
+			$('#distanceMark').hide();
+			$('#timeMark').show();
+		}
+		//change map with color for different type
+		if($("#dropdownList").val()){
+			var markers = [];
+			
+			$.ajax
+	    	({
+	    		type: "GET",
+	    		//dataType : 'json',
+	    		//url: 'https://dev.smartcommunitylab.it/cedus/api/cover/education',
+	    		url:'../api/cover/education',
+	    		data: {ordine:$('#levelText').text(),tipologia:$("#dropdownList").val(),filter:radioValSelect} ,
+	    		success: function (data) {
+	    		//console.log("data name:",data['tuList']);
+	    		$.each(data['tuList'],function(key, val){
+	    			//console.log("data geocode:",val['geocode'][0]);
+	    			markers.push({
+	    			  lat: val['geocode'][1],
+	    			  lng: val['geocode'][0],
+	    			  name: val['name']
+	    			});
+	    		});
+	    		
+	    		initMap(markers,data['districtMap']);	
+	    		},
+	    		failure: function() {console.log("Error!");}
+	    	});
+		}
+	});
 });
 function change_div(level_text){
 	var markers = [];
 	$('#select_btn').hide();
 	$('#select_type').show();
 	$('#levelText').text(level_text);
-	//console.log("dropdown value:",$("#dropdownList").val());
+	/*
 	//ajax call for markers filter by level(on click level button)
 	$.ajax
 	({
@@ -77,29 +117,28 @@ function change_div(level_text){
 			});
 		});
 		
-		initMap(markers,data['districtMap']);	
+		//initMap(markers,data['districtMap']);	
 		},
 		failure: function() {alert("Error!");}
 	});
+	*/
+	$('select#dropdownList').empty();
 	//ajax call for tipologia (fullfill the dropdown box)
 	$.ajax
 	({
 		type: "GET",
 		//dataType : 'json',
 		//url: 'https://dev.smartcommunitylab.it/cedus/api/params/Tipologie',
-		url:'../api/params/tipologie',
-		data: '' ,
+		url:'../api/params/tipologieForOrdine',
+		data: {ordine:level_text} ,
 		success: function (data) {
-		//console.log("data tipologia:",data);
 		$("#dropdownList").append("<option value=''>Tipologia di selezione</option>");
 		$.each(data,function(key, val){
-			//console.log("data geocode:",val);
-			//$("#dropdownList").append("<li><a href='#'>"+val+"</a></li>");
 			$("#dropdownList").append("<option value='"+val+"'>"+val+"</option>");
 		});
 			
 		},
-		failure: function() {alert("Error!");}
+		failure: function() {console.log("Error!");}
 	});
 }
 function back_div(){
@@ -212,85 +251,96 @@ function initMap(markers,districtMap) {
 		var polyColor;
 		var decodePaths=[];
 		//decode.push(google.maps.geometry.encoding.decodePath(val['enString'][0]));
-		decodePaths.push(google.maps.geometry.encoding.decodePath(val['enString'][0]));
+		if(val['enString'][0]){
+			decodePaths.push(google.maps.geometry.encoding.decodePath(val['enString'][0]));
+		}
+		if(val['enString'][1]){
+			decodePaths.push(google.maps.geometry.encoding.decodePath(val['enString'][1]));				
+		}
 		if(districtMap){
 			var codiceIstat="0"+val['PRO_COM'];
+			polyColor=getPolyColor(districtMap[codiceIstat]);
+			/*
+			var radioVal=$( "input.radioOption:checked" ).val();
+			var distMap;
+			if(radioVal=="CAR_DISTANCE"){
+				distMap="carDistance";
+			}else if(radioVal=="CAR_TIME"){
+				distMap="carTime";
+			}else if(radioVal=="TRANSIT_DISTANCE"){
+				distMap="transitDistance";
+			}else if(radioVal=="TRANSIT_TIME"){
+				distMap="transitTime";
+			}
 			//console.log("data 4 local file:",val['PRO_COM']);
 			//console.log("districtMap:",districtMap[codiceIstat]);
 			if(districtMap[codiceIstat]){
-				if(districtMap[codiceIstat]['carDistance']<=50){
-					polyColor="#000000";
-				}else if(districtMap[codiceIstat]['carDistance']>50 && districtMap[codiceIstat]['carDistance']<=100){
-					polyColor="#555555";
-				}else if(districtMap[codiceIstat]['carDistance']>100 && districtMap[codiceIstat]['carDistance']<=150){
-					polyColor="#666666";
-				}else if(districtMap[codiceIstat]['carDistance']>150 && districtMap[codiceIstat]['carDistance']<=200){
-					polyColor="#777777";
+				if(districtMap[codiceIstat][distMap]<=20){
+					polyColor="#40C144";
+				}else if(districtMap[codiceIstat][distMap]>20 && districtMap[codiceIstat][distMap]<=40){
+					polyColor="#E4EA19";
+				}else if(districtMap[codiceIstat][distMap]>40 && districtMap[codiceIstat][distMap]<=100){
+					polyColor="#FFA500";
+				}else if(districtMap[codiceIstat][distMap]>100 ){
+					polyColor="#FF0000";
 				}else{
+					console.log("districtMap:",districtMap[codiceIstat]);
 					polyColor="#999999";
 				}
 			}else{
 				//close to white color
 				polyColor="#FFFFFF";
 			}
+			*/
 		}
-		//console.log("districtMap:",districtMap);
-		/*
-		if(markers){
-			//console.log("markers:",markers);
-			$.each(markers,function(key2, val2){
-				//check  lat:val['lat'],lng:val['lng'] is in map and put color
-				var coord1 = new google.maps.LatLng(val2['lat'],val2['lng']);
-				var bermudaTriangle = new google.maps.Polygon({paths: google.maps.geometry.encoding.decodePath(val['enString'][0])});
-				if (google.maps.geometry.poly.containsLocation(coord1, bermudaTriangle)) {
-					console.log("marker is inside the polygon");
-					polyColor="#000000";
-					return false; 
-				} else {
-					console.log("marker is not inside the polygon");
-					polyColor="#878787";
-				}
-			});
-			
-		}
-		polygons[key] = new google.maps.Polygon({
-		    paths: google.maps.geometry.encoding.decodePath(val['enString'][0]),
-		    strokeColor: '#FFFFFF',
-		    strokeOpacity: 0.8,
-		    strokeWeight: 2,
-		    fillColor: polyColor,
-		    fillOpacity: 1
-	  	});
-		polygons[key].setMap(map);
-		*/
-		if(val['enString'][1]){
-			//decode.push(google.maps.geometry.encoding.decodePath(val['enString'][1]));
-			decodePaths.push(google.maps.geometry.encoding.decodePath(val['enString'][1]));
-						
-		}
+		
+		
 		polygons[key] = new google.maps.Polygon({
 		    paths: decodePaths,
 		    strokeColor: '#FFFFFF',
 		    strokeOpacity: 0.8,
 		    strokeWeight: 2,
 		    fillColor: polyColor,
-		    fillOpacity: 1
+		    fillOpacity: 0.8
 	  	});
 		polygons[key].setMap(map);
 	});
-	/*
-	// Construct the polygon.
-  	var polygons = new google.maps.Polygon({
-	    paths: decode,
-	    strokeColor: '#FFFFFF',
-	    strokeOpacity: 0.8,
-	    strokeWeight: 2,
-	    fillColor: '#000000',
-	    fillOpacity: 1
-  	});
-  	
-  	polygons.setMap(map);
-  	*/
+	
 	google.maps.event.trigger(map, 'resize');
   
+}
+
+//function return the polygons color
+function getPolyColor(districtMap){
+	var radioVal=$( "input.radioOption:checked" ).val();
+	var polyColor,distMap;
+	if(radioVal=="CAR_DISTANCE"){
+		distMap="carDistance";
+	}else if(radioVal=="CAR_TIME"){
+		distMap="carTime";
+	}else if(radioVal=="TRANSIT_DISTANCE"){
+		distMap="transitDistance";
+	}else if(radioVal=="TRANSIT_TIME"){
+		distMap="transitTime";
 	}
+	//console.log("data 4 local file:",val['PRO_COM']);
+	//console.log("districtMap:",districtMap[codiceIstat]);
+	if(districtMap){
+		if(districtMap[distMap]<=20){
+			polyColor="#40C144";
+		}else if(districtMap[distMap]>20 && districtMap[distMap]<=40){
+			polyColor="#E4EA19";
+		}else if(districtMap[distMap]>40 && districtMap[distMap]<=100){
+			polyColor="#FFA500";
+		}else if(districtMap[distMap]>100 ){
+			polyColor="#FF0000";
+		}else{
+			console.log("districtMap:",districtMap);
+			polyColor="#999999";
+		}
+	}else{
+		//close to white color
+		polyColor="#FFFFFF";
+	}
+	return polyColor;
+}
