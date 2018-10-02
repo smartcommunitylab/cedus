3728a675918a388a43a2cd220c8f38edb8f7422d
Stefano Cudini <stefano.cudini@gmail.com>
2018-10-02 11:26:24 +0200

    change start path

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# mvn clean install -Dmaven.test.skip=true
-java -jar cedus-engine/target/cedus.jar
+cd cedus-engine
+
+mvn clean install -Dmaven.test.skip=true
+
+java -jar target/cedus.jar
 
 echo "http://localhost:6050/cedus/web/student"
