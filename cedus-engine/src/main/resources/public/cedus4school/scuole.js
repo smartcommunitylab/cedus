2366befdfa7a7d994c4bff1c98dd41ca6af44a4a
Stefano Cudini <stefano.cudini@gmail.com>
2018-12-05 11:20:08 +0100

    update map point of interest in scuole section

--- cedus-engine/src/main/resources/public/cedus4school/scuole.js
+++ cedus-engine/src/main/resources/public/cedus4school/scuole.js
@@ -80389,7 +80389,7 @@ module.exports.POLAR_RADIUS = 6356752.3142;
 },{}],180:[function(require,module,exports){
 module.exports={
   "name": "osm4schools",
-  "version": "3.5.0",
+  "version": "3.7.0",
   "description": "mappa delle scuole",
   "author": {
     "name": "Stefano Cudini",
@@ -82484,9 +82484,10 @@ module.exports = {
   		width: 500,
   		height: 500,
   		overpassTags: [
-  			"amenity=parking",
-			"amenity=bar",
-			"highway=restaurant"
+  			{tag:"amenity=bar",color:'orange',label:"Bar"},
+  			{tag:"amenity=parking",color:'DodgerBlue',label:"Parcheggi"},
+			{tag:"amenity=post_office",color:'brown',label:"Uffici Postali"},
+			{tag:"amenity=school",color:'red',label:"Altre Scuole"},
 		]
   	},
 
@@ -82500,9 +82501,6 @@ module.exports = {
 
 		self.$el.width(self.config.width).height(self.config.height);
 
-		self.tmplLegend = H.compile($('#tmpl_legend').html());
-		$('#poi_legend').append( self.tmplLegend(iso.getLegend()) );
-
         L.Icon.Default.imagePath = location.href.split('/').slice(0,-1).join('/')+'/images/';
 
 		self.map = L.map(el, utils.getMapOpts());
@@ -82511,9 +82509,86 @@ module.exports = {
 		self.marker = L.marker([0,0]).addTo(self.map).bindTooltip('',{direction:'top', offset: L.point(0,-10)});
 
 		self._overpassKeys = _.uniq(_.map(self.config.overpassTags, function(t) {
-			return t.split('=')[0];
+			return t.tag.split('=')[0];
 		}));
 	
+		self.legendPoi = new L.Control({position:'bottomleft'});
+		self.legendPoi.onAdd = function(map) {
+			var div = L.DomUtil.create('div','legend');
+			var ll = _.map(self.config.overpassTags, function(v,k) {
+				return '<b style="color:'+v.color+'">&#11044;</b> <span>'+v.label+'</span>';
+			});
+			div.innerHTML = ll.join("<br />\n");
+			return div;
+		};
+		self.legendPoi.addTo(self.map);
+		
+		self.legendIso = new L.Control({position:'bottomleft'});
+		self.legendIso.onAdd = function(map) {
+			var div = L.DomUtil.create('div','legend');
+			var ll = _.map(iso.getLegend().list, function(v,k) {
+				var t = v.value+' minuti';
+				return '<b style="color:'+v.color+'">&#11035;</b> <span>'+t+'</span>';
+			});
+			div.innerHTML = ll.join("<br />\n");
+			return div;
+		};
+		self.legendIso.addTo(self.map);
+		
+		self.layerPoi = L.geoJSON([], {
+			pointToLayer: function(f, ll) {
+				
+				var color = 'white';
+				
+				_.each(self.config.overpassTags, function(v,k) {
+					var tag = v.tag.split('=')[1];
+					if(tag===f.properties.amenity) 
+						color = v.color;
+				});
+
+				return L.circleMarker(ll, {
+					radius: 5,
+					weight: 1,
+					opacity: 1,
+					color:'white',
+					fillOpacity: 0.8,
+					fillColor: color
+				})
+			},
+			onEachFeature: function(feature, layer) {
+
+				layer.on('mouseover', function(e) {
+
+					var f = e.target.feature;
+
+					var keys = _.map(f.properties, function(v,k) {
+						if(_.contains(self._overpassKeys, k))
+							return v.replace('_',' ');
+					});
+
+					if(!f.properties.time) {
+						self.layerIso.eachLayer(function(l) {
+							if(geoutils.pointInPolygon(f.geometry, l.feature.geometry)) {
+								f.properties.time = Math.floor(l.feature.properties.value / 60); 
+							}
+						});
+					}
+
+					f.properties.label = _.compact(keys).join(', ');
+
+					layer.bindTooltip( config.tmpls.map_popup(f.properties) ).openTooltip();
+				});
+
+			},
+			attribution:'<a href="http://overpass-api.de/">OverpassApi</a>',
+		}).on('add', function(e) {
+			self.map.addControl(self.legendPoi);
+			e.target.bringToFront();
+		}).on('remove', function(e) {
+			self.map.removeControl(self.legendPoi);
+		})
+		.addTo(self.map);	
+
 		self.layerIso = L.geoJSON([], {
 			style: function(f) {
 				return {
@@ -82535,44 +82610,12 @@ module.exports = {
 				})
 			}*/
 			attribution:'<a href="https://openrouteservice.org">OpenRouteService</a>',
+		}).on('add', function(e) {
+			self.map.addControl(self.legendIso);
+			e.target.bringToBack();
+		}).on('remove', function(e) {
+			self.map.removeControl(self.legendIso);
 		}).addTo(self.map);
-
-		self.layerPoi = L.geoJSON([], {
-			pointToLayer: function(f, ll) {
-				return L.circleMarker(ll, {
-					radius: 5,
-					weight: 2,
-					opacity: 1,
-					fillOpacity: 1,
-					fillColor: '#fff'
-				})
-			},
-			onEachFeature: function(feature, layer) {
-
-				layer.on('mouseover', function(e) {
-
-					var f = e.target.feature;
-
-					var keys = _.map(f.properties, function(v,k) {
-						if(_.contains(self._overpassKeys, k))
-							return v.replace('_',' ');
-					});
-
-					if(!f.properties.time) {
-						self.layerIso.eachLayer(function(l) {
-							if(geoutils.pointInPolygon(f.geometry, l.feature.geometry))
-								f.properties.time = Math.floor(l.feature.properties.value / 60); 
-						});
-					}
-
-					f.properties.label = _.compact(keys).join(', ');
-
-					layer.bindTooltip( config.tmpls.map_popup(f.properties) ).openTooltip();
-				});
-
-			},
-			attribution:'<a href="http://overpass-api.de/">OverpassApi</a>',
-		}).addTo(self.map);	
 
 		L.control.layers(null, {
 			'Luoghi di interesse': self.layerPoi,
@@ -82601,8 +82644,9 @@ module.exports = {
 
 			var bbox = utils.bufferLoc(obj.loc, 200);
 
-			if(geoIso.bbox)
+			if(geoIso.bbox) {
 				bbox = L.latLngBounds(L.latLng(geoIso.bbox[1],geoIso.bbox[0]), L.latLng(geoIso.bbox[3],geoIso.bbox[2]) );
+			}
 
 			self.map.fitBounds(bbox.pad(-0.9));
 
@@ -82619,7 +82663,7 @@ module.exports = {
 				
 				self.layerPoi.addData(geoPoi);
 
-			}, self.config.overpassTags);
+			}, _.pluck(self.config.overpassTags,'tag'));
 
 		});
 	}
